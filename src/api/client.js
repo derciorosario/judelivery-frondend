0369b464a1fac77bf4d968aea27819b23e9e0682
derciorosario <derciorosario55@gmail.com>
2026-08-06 17:31:20 +0200

    .

--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const env = "dev";
+export const env = "pro";
 import { Capacitor, CapacitorHttp } from '@capacitor/core';
 export const isNative = Capacitor.isNativePlatform();
 export const APP_VERSION="1.0"
